78acc874c761ef44a714d3b85c31af9597a93f1b
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-04-02 12:09:30 +0400

    Services created and added to the system.

---
+++ La-Grazia/La-Grazia/Services/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace La_Grazia.Services
+{
+    public interface IEmailService
+    {
+        void Send(string to, string subject, string html);
+    }
+
+    public class EmailService : IEmailService
+    {
+        public void Send(string to, string subject, string html)
+        {
+            // creating message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("lagrazia@yandex.com"));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = html };
+
+            // sending email
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.yandex.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("lagrazia@yandex.com", "123321");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
---
+++ La-Grazia/La-Grazia/Services/LayoutService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using La_Grazia.Database;
+using La_Grazia.Database.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using La_Grazia.ViewModels;
+using Newtonsoft.Json;
+
+namespace La_Grazia.Services
+{
+    public class LayoutService
+    {
+        private readonly DataContext _dataContext;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly UserManager<User> _userManager;
+
+        public LayoutService(DataContext dataContext, IHttpContextAccessor contextAccessor, UserManager<User> userManager)
+        {
+            _dataContext = dataContext;
+            _contextAccessor = contextAccessor;
+            _userManager = userManager;
+        }
+
+        public Setting GetSetting()
+        {
+            return _dataContext.Settings.FirstOrDefault();
+        }
+
+        public List<BasketProductViewModel> GetBasketProducts()
+        {
+            List<BasketProductViewModel> items = new List<BasketProductViewModel>();
+
+            User member = null;
+
+            if (_contextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                member = _userManager.Users.FirstOrDefault(x => x.UserName == _contextAccessor.HttpContext.User.Identity.Name && !x.IsAdmin);
+            }
+
+            if (member == null)
+            {
+                var itemsStr = _contextAccessor.HttpContext.Request.Cookies["Products"];
+
+                if (itemsStr != null)
+                {
+                    items = JsonConvert.DeserializeObject<List<BasketProductViewModel>>(itemsStr);
+
+                    foreach (var item in items)
+                    {
+                        Product product = _dataContext.Products.Include(c => c.ProductImages).FirstOrDefault(x => x.Id == item.ProductId);
+                        if (product != null)
+                        {
+                            item.Name = product.Name;
+                            item.Price = product.SalePrice;
+                            item.Image = product.ProductImages.FirstOrDefault()?.Image;
+                        }
+                    }
+                }
+            }
+
+            else
+            {
+                List<BasketProduct> BasketProducts = _dataContext.BasketProducts.Include(x => x.Product).ThenInclude(x => x.ProductImages).Where(x => x.UserId == member.Id).ToList();
+                items = BasketProducts.Select(x => new BasketProductViewModel
+                {
+                    ProductId = x.ProductId,
+                    Count = x.Count,
+                    Image = x.Product.ProductImages.FirstOrDefault()?.Image,
+                    Name = x.Product.Name,
+                    Price = x.Product.SalePrice
+                }).ToList();
+            }
+
+            return items;
+        }
+
+        public List<WishlistProductViewModel> GetWishlistItems()
+        {
+            List<WishlistProductViewModel> items = new List<WishlistProductViewModel>();
+
+            User member = null;
+
+            if (_contextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                member = _userManager.Users.FirstOrDefault(x => x.UserName == _contextAccessor.HttpContext.User.Identity.Name && !x.IsAdmin);
+            }
+
+            if (member == null)
+            {
+                var itemsStr = _contextAccessor.HttpContext.Request.Cookies["Wishlist"];
+
+                if (itemsStr != null)
+                {
+                    items = JsonConvert.DeserializeObject<List<WishlistProductViewModel>>(itemsStr);
+
+                    foreach (var item in items)
+                    {
+                        Product product = _dataContext.Products.Include(c => c.ProductImages).FirstOrDefault(x => x.Id == item.ProductId);
+
+                        if (product != null)
+                        {
+                            item.Name = product.Name;
+                            item.Price = product.SalePrice;
+                            item.Image = product.ProductImages.FirstOrDefault()?.Image;
+                        }
+                    }
+                }
+            }
+
+            else
+            {
+                List<WishlistProduct> wishlistItems = _dataContext.WishlistProducts.Include(x => x.Product).ThenInclude(x => x.ProductImages).Where(x => x.UserId == member.Id).ToList();
+                items = wishlistItems.Select(x => new WishlistProductViewModel
+                {
+                    ProductId = x.ProductId,
+                    Image = x.Product.ProductImages.FirstOrDefault()?.Image,
+                    Name = x.Product.Name,
+                    Price = x.Product.SalePrice
+                }).ToList();
+            }
+
+            return items;
+        }
+    }
+}
